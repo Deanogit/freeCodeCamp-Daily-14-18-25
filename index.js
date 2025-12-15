7335b65fcc33526ecba1bd49d7db91909f5af3e7
Dean Howe <hideanhowe@gmail.com>
2025-12-15 15:38:05 +0000

    oneliner

--- index.js
+++ index.js
@@ -4,20 +4,20 @@
 // Non-alphabetical characters should remain unchanged (except for spaces).
 
 function spaceJam(s) {
-  const regex = /[\s]+/g;
+  const regex = /\s/g;
   console.log(s);
-  const trimmed = s.trim();
-  const filtered = trimmed.replace(regex, '');
-  const split = filtered.split('');
-  const resultArr = [];
-  console.log(split);
-  split.forEach((x) => {
-    resultArr.push(x.toUpperCase());
-  });
-  console.log(resultArr);
-  const resultStr = resultArr.join('  ');
-  console.log(resultStr);
+  // const trimmed = s.trim();
+  const filtered = s.replace(regex, '');
+  const upper = filtered.toUpperCase();
+  // const split = upper.split('');
+  const resultStr = upper.split('').join('  ');
+  // console.log(resultArr);
+  // const resultStr = resultArr.join('  ');
+  //console.log(resultStr);
   return resultStr;
 }
 
 spaceJam('   free   Code   Camp   '); // "F  R  E  E  C  O  D  E  C  A  M  P"
+
+/// const spaceJam = s =>
+s.replace(/\s/g, '').toUpperCase().split('').join('  ');
