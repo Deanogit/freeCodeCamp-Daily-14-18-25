c45e9ec67c78a1b750301251bc242f0ff4ecd5d6
Dean Howe <hideanhowe@gmail.com>
2025-12-15 15:12:31 +0000

    testing

--- index.js
+++ index.js
@@ -2,3 +2,17 @@
 // Given a string, remove all spaces from the string, insert two spaces between every character, convert all alphabetical letters to uppercase, and return the result.
 
 // Non-alphabetical characters should remain unchanged (except for spaces).
+
+function spaceJam(s) {
+  console.log(s);
+  const split = s.split('');
+  const resultArr = [];
+  console.log(split);
+  split.forEach((x) => {
+    resultArr.push(x.toUpperCase());
+  });
+  console.log(resultArr);
+  const resultStr = resultArr.join('  ');
+  console.log(resultStr);
+  return resultStr;
+}
